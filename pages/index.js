1c2ec33f1f278254293fa26635cc044430fff1b8
Codehash001 <codehashblockchain@gmail.com>
2026-03-30 13:38:33 +0530

    fix twitter

--- pages/index.js
+++ pages/index.js
@@ -328,7 +328,7 @@ export default function Home() {
             { href: 'https://etherscan.io/address/0x1f1F860A0A3bEC70107d31CFDcAEfF2E59631a27', icon: 'etherscansvg.svg', label: 'Etherscan' },
             { href: 'https://discord.com/invite/nurws4nAgE', icon: 'discord.svg', label: 'Discord' },
             { href: 'https://instagram.com/moliae8', icon: 'instagram.svg', label: 'Instagram' },
-            { href: 'https://x.com/moliae', icon: 'twitter.svg', label: 'X' },
+            // { href: 'https://x.com/moliae', icon: 'twitter.svg', label: 'X' },
             { href: 'https://facebook.com/moliae', icon: 'facebook.svg', label: 'Facebook' },
           ].map((social, idx) => (
             <a
@@ -424,7 +424,7 @@ export default function Home() {
           { href: 'https://etherscan.io/address/0x1f1F860A0A3bEC70107d31CFDcAEfF2E59631a27', icon: 'etherscansvg.svg', label: 'Etherscan' },
           { href: 'https://discord.com/invite/nurws4nAgE', icon: 'discord.svg', label: 'Discord' },
           { href: 'https://instagram.com/moliae8', icon: 'instagram.svg', label: 'Instagram' },
-          { href: 'https://x.com/moliae', icon: 'twitter.svg', label: 'X' },
+          // { href: 'https://x.com/moliae', icon: 'twitter.svg', label: 'X' },
           { href: 'https://facebook.com/moliae', icon: 'facebook.svg', label: 'Facebook' },
           ].map((social, idx) => (
             <a
