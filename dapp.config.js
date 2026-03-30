0334181b910a8b91acaeb53f27a658ab9580e103
Codehash001 <codehashblockchain@gmail.com>
2026-03-30 12:54:38 +0530

    fix mobile responsiveness

--- dapp.config.js
+++ dapp.config.js
@@ -18,7 +18,7 @@ const onboardOptions = {
   networkId: 1, // mainnet id
   darkMode: true,
   walletSelect: {
-    description:'Plaese select a wallet',
+    description:'Please select a wallet',
     wallets: [
       { walletName: 'metamask', preferred: true },
       { walletName: 'coinbase', preferred: true },
